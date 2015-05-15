983b0749d93ceaf259f01d9384db39856f5eef53
Jinhyuk Lee <eces@mstock.org>
2015-05-15 12:04:39 +0900

    Fix

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -17,7 +17,7 @@ EB_BUCKET=zari-deploy
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name zari-env \
+aws elasticbeanstalk create-application-version --application-name zari \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
