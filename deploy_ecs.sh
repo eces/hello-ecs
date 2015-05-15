7b4ea0d2e3a581aea063c4419e1a18219d24ed05
Jinhyuk Lee <eces@mstock.org>
2015-05-15 15:48:10 +0900

    Add

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -2,24 +2,24 @@
 
 SHA1=$1
 
-# aws ecs register-task-definition --cli-input-json file://$(pwd)/ecs_task_definition.json
-
-# aws ecs list-task-definitions
-
-# aws ecs run-task --cluster default --task-definition hello-ecs --count 2
-
-# aws ecs list-tasks --cluster default
-# 
-
 docker push quay.io/eces/hello-ecs
 
-EB_BUCKET=zari-deploy
-DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
-sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name zari \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws ecs register-task-definition --cli-input-json file://$(pwd)/ecs_task_definition.json
+aws ecs run-task --cluster default --task-definition zari-task --count 1
+aws ecs list-tasks --cluster default
+# aws ecs list-task-definitions
 
-# Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name zari-env \
-    --version-label $SHA1
+
+# 
+
+
+# EB_BUCKET=zari-deploy
+# DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+# sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+# aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+# aws elasticbeanstalk create-application-version --application-name zari \
+#   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# # Update Elastic Beanstalk environment to new version
+# aws elasticbeanstalk update-environment --environment-name zari-env \
+#     --version-label $SHA1
