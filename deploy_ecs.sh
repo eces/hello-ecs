946760825385415abdc371d60c20e0ee39742c17
Jinhyuk Lee <eces@mstock.org>
2015-05-15 11:15:41 +0900

    Deploy to beanstalk

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -4,10 +4,21 @@ SHA1=$1
 
 docker push quay.io/eces/hello-ecs
 
-aws ecs register-task-definition --cli-input-json file://$(pwd)/ecs_task_definition.json
+# aws ecs register-task-definition --cli-input-json file://$(pwd)/ecs_task_definition.json
 
-aws ecs list-task-definitions
+# aws ecs list-task-definitions
 
-aws ecs run-task --cluster default --task-definition hello-ecs --count 2
+# aws ecs run-task --cluster default --task-definition hello-ecs --count 2
 
-aws ecs list-tasks --cluster default
+# aws ecs list-tasks --cluster default
+
+EB_BUCKET=zari-deploy
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name multiContainerDocke-env \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name multiContainerDocke-env \
+    --version-label $SHA1
