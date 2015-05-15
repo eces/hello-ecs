700f1e7029b867fa7b8351ca7620d185fde35f10
Jinhyuk Lee <eces@mstock.org>
2015-05-15 11:23:12 +0900

    Fix

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -2,8 +2,6 @@
 
 SHA1=$1
 
-docker push quay.io/eces/hello-ecs
-
 # aws ecs register-task-definition --cli-input-json file://$(pwd)/ecs_task_definition.json
 
 # aws ecs list-task-definitions
@@ -16,9 +14,9 @@ EB_BUCKET=zari-deploy
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name multiContainerDocke-env \
+aws elasticbeanstalk create-application-version --application-name myFirstElasti-cvjus-env \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name multiContainerDocke-env \
+aws elasticbeanstalk update-environment --environment-name myFirstElasti-cvjus-env \
     --version-label $SHA1
