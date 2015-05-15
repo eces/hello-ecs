2a9b7382783c5ab7571838a4dc169a631c42b88f
Jinhyuk Lee <eces@mstock.org>
2015-05-15 11:26:53 +0900

    Do

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -9,14 +9,17 @@ SHA1=$1
 # aws ecs run-task --cluster default --task-definition hello-ecs --count 2
 
 # aws ecs list-tasks --cluster default
+# 
+
+docker push quay.io/eces/hello-ecs
 
 EB_BUCKET=zari-deploy
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name myFirstElasti-cvjus-env \
+aws elasticbeanstalk create-application-version --application-name nodejs-env \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name myFirstElasti-cvjus-env \
+aws elasticbeanstalk update-environment --environment-name nodejs-env \
     --version-label $SHA1
